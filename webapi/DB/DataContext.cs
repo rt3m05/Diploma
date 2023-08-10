f5a563de670f0d1fee06f727667f9f0efd8c39ed
sanee4ekk <sanee4ekk@gmail.com>
2023-08-10 22:24:15 +0300

    Create db context and add db autocreate

---
+++ webapi/DB/DataContext.cs
@@ -0,0 +1,83 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Options;
+using MySql.Data.MySqlClient;
+using System.Data;
+using webapi.Controllers;
+
+namespace webapi.DB
+{
+    public class DataContext
+    {
+        private DbSettings _dbSettings;
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public DataContext(IOptions<DbSettings> dbSettings, ILogger<WeatherForecastController> logger)
+        {
+            _dbSettings = dbSettings.Value;
+            _logger = logger;
+        }
+
+        public IDbConnection? CreateConnection()
+        {
+            try
+            {
+                var connectionString = $"Server={_dbSettings.Server}; Database={_dbSettings.Database}; Uid={_dbSettings.UserId}; Pwd={_dbSettings.Password};";
+                return new MySqlConnection(connectionString);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                return null;
+            }
+        }
+
+        public async Task Init()
+        {
+            await _initDatabase();
+            await _initTables();
+        }
+
+        //Create database if it doesn't exist
+        private async Task _initDatabase()
+        {
+            try
+            {
+                var connectionString = $"Server={_dbSettings.Server}; Uid={_dbSettings.UserId}; Pwd={_dbSettings.Password};";
+                using var connection = new MySqlConnection(connectionString);
+                var sql = $"CREATE DATABASE IF NOT EXISTS {_dbSettings.Database};";
+                await connection.ExecuteAsync(sql);
+            } catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+            }
+        }
+
+        //Create tables if they don't exist
+        private async Task _initTables()
+        {
+            using var connection = CreateConnection();
+            if (connection == null)
+                return;
+
+            //await _initUsers();
+
+            //async Task _initUsers()
+            //{
+            //    var sql = """
+            //    IF OBJECT_ID('Users', 'U') IS NULL
+            //    CREATE TABLE Users (
+            //        Id INT NOT NULL PRIMARY KEY IDENTITY,
+            //        Title NVARCHAR(MAX),
+            //        FirstName NVARCHAR(MAX),
+            //        LastName NVARCHAR(MAX),
+            //        Email NVARCHAR(MAX),
+            //        Role INT,
+            //        PasswordHash NVARCHAR(MAX)
+            //    );
+            //""";
+            //    await connection.ExecuteAsync(sql);
+            //}
+        }
+    }
+}
--- webapi/Program.cs
+++ webapi/Program.cs
@@ -1,14 +1,28 @@
+using webapi.DB;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+//Configure DB settings
+builder.Services.Configure<DbSettings>(builder.Configuration.GetSection("DbSettings"));
+
+//Add DB context
+builder.Services.AddSingleton<DataContext>();
+
 var app = builder.Build();
+
+//Create DB and tables if they don`t exists
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<DataContext>();
+    await context.Init();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -22,5 +36,11 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+//CORS
+//app.UseCors(x => x
+//        .AllowAnyOrigin()
+//        .AllowAnyMethod()
+//        .AllowAnyHeader());
 
 app.Run();
