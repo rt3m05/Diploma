3251bd6a39bce5ded72f4b7a01bd001161024d7c
rt3m05 <ciga_we95@student.itstep.org>
2023-10-10 22:45:37 +0200

    Added adaptability to projects page

--- reactapp/src/pages/ProjectsPage.jsx
+++ reactapp/src/pages/ProjectsPage.jsx
@@ -4,12 +4,92 @@ import ProjIcon from "../images2/proj.png";
 import PlusIcon from "../images2/plus.png";
 import TrashIcon from "../images2/trash.png";
 import CompanyIcon from "../images2/companyLogo.png";
+
+import Bell2Icon from "../images2/bell2.png";
+import Accepted2Icon from "../images2/accepted2.png";
+import Question2Icon from "../images2/question2.png";
+import Menu2Icon from "../images2/menu2.png";
+import SearchIcon from "../images2/search2.png";
+import Search2Icon from "../images2/search.png";
+
+import ArrowIcon from '../images2/arrow.png';
+import Plus2Icon from "../images2/plus2.png";
+import Star2Icon from "../images2/star2.png";
+import PersonIcon from "../images2/person.png";
+import GroupIcon from "../images2/group.png";
+import TemplatesIcon from "../images2/templates.png";
+import RecentIcon from "../images2/recent.png";
+
 import '../styles2/ProjectsPage/mainPage.css';
 
 function App() {
+  const menuAdd = () => {
+    let leftMenu = document.querySelector(".Workspace_leftMenu");
+    leftMenu.classList.toggle("disable");
+    leftMenu.classList.toggle("active");
+};
+const menuRemove = () => {
+    let leftMenu = document.querySelector(".Workspace_leftMenu");
+    leftMenu.classList.toggle("active");
+    leftMenu.classList.toggle("disable");
+};
   return (
     <div className="App">
+
+    <div className='Workspace_leftMenu disable'>
+
+      <div className='Workspace_lLogo'>
+          <img src={CompanyIcon} alt="CompanyIcon" className='Workspace_logo'/>
+          <p>Daily</p>
+          <img src={ArrowIcon} alt="ArrowIcon" className='Workspace_back' onClick={menuRemove}/>
+      </div>
+
+      <div className='Workspace_leftNavBar'>
+          <div className='Workspace_leftNavBarDivActive'>
+              <img src={PersonIcon} alt='PersonIcon'/>
+              <p>Основна</p>
+          </div>
+          <div>
+              <img src={GroupIcon} alt='GroupIcon'/>
+              <p>Спільний доступ</p>
+          </div>
+          <div>
+              <img src={Star2Icon} alt='Star2Icon'/>
+              <p>Обрані</p>
+          </div>
+          <div>
+              <img src={RecentIcon} alt='RecentIcon'/>
+              <p>Нещодавні</p>
+          </div>
+          <div>
+              <img src={TemplatesIcon} alt='TemplatesIcon'/>
+              <p>Галерея шаблонів</p>
+          </div>
+          <div>
+              <img src={Plus2Icon} alt='Plus2Icon' className='Workspace_addWorkplace'/>
+              <p>Робочі області</p>
+          </div>
+      </div>
+
+      <div className='Workspace_trash'>
+          <img src={TrashIcon} alt='Plus2Icon'/>
+          <h3>Кошик</h3>
+      </div>
+
+    </div>
+      
+    
       <div className='App_left'>
+            <div className='App_navBar2'>
+                <img src={Menu2Icon} alt="menu" className='App_menuIcon2' onClick={menuAdd}/>
+                <div>
+                    <img src={Accepted2Icon} alt="accepted" />
+                    <img src={Bell2Icon} alt="bell" />
+                    <img src={Search2Icon} alt="Search2Icon" />
+                    <img src={Question2Icon} alt="QuestionIcon" />  
+                    <img src={CompanyIcon} alt="CompanyIcon" style={{width:'50px', height:'50px'}}/>  
+                </div>     
+            </div>
         <div className='App_Lhead'>
 
           <div className='App_group1'>
