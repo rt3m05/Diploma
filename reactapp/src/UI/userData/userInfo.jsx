eecb07ec37110d1bacfc974087a28d6da7275c43
Олександр <ponomarenko_sasha1985@ukr.net>
2023-11-07 14:01:01 +0200

    add request userInfo

--- reactapp/src/UI/userData/userInfo.jsx
+++ reactapp/src/UI/userData/userInfo.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import UserPhoto from './userPhoto';
 
 import UserModal from './userModal';
@@ -8,49 +8,55 @@ import UserEmail from './usermail';
 
 const UserInfo = () => {
     const [email, setEmail] = useState(''); 
-    const [imageUrl, setImageUrl] = useState(''); 
+    const [imageUrl, setImageUrl] = useState(); 
     const [isModalOpen, setIsModalOpen] = useState(false);
     const [userData, setUserData] = useState('');
-
+    
     const handleClick = () => {
         setIsModalOpen(true);
     }
-    const request = async () => {
-        const token = document.cookie
-            .split("; ")
-            .find((row) => row.startsWith("token="))
-            ?.split("=")[1];
-        try {
-            const response = await fetch("https://localhost:7023/api/User", {
-                method: "GET",
-                headers: {
-                    "Authorization": `Bearer ${token}`
-                },
-            });
-      
-            if (!response.ok) {
-                throw new Error(`Ошибка запроса: ${response.status}`);
+    useEffect(() => {
+        const request = async () => {
+            const token = document.cookie
+                .split("; ")
+                .find((row) => row.startsWith("token="))
+                ?.split("=")[1];
+            try {
+                const response = await fetch("https://localhost:7023/api/Users", {
+                    method: "GET",
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                    },
+                });
+
+                if (!response.ok) {
+                    throw new Error(`Ошибка запроса: ${response.status}`);
+                }
+
+                setUserData(await response.json());
+            } catch (error) {
+                console.error("Ошибка:", error.message);
             }
-            
-            setUserData(await response.json());            
-        }
-        catch (error) {
-            console.error('Ошибка:', error.message);
-        }
-    }
-    request();
-    console.log(userData);
-    if (userData) {
+        };
+        request();
+    }, []);
+    console.dir(userData);
+    
+    useEffect(() => {
+      if (userData) {
         setEmail(userData.email);
         setImageUrl(userData.image);
-    }
+      }
+    }, [userData]);
 
+    console.log("email =" + email);
+    console.log("image =" + imageUrl);
     if (!imageUrl) {
         setImageUrl(Avatar);
     }
 
     if (email == "") {
-        setEmail("Hello World!");
+        setEmail("Djon Dou");
     }
 
   return (
